d06c20a74e8c4ec1aa5fe4617dd5fe0b96cebb1a
rohitsehgal <Rockstar@1>
2017-07-12 19:32:06 +0800

    - basic file connection

--- src/main/java/DBManager.java
+++ src/main/java/DBManager.java
@@ -1,5 +1,90 @@
+import java.io.File;
+import java.sql.*;
+
 /**
  * Created by rohitsehgal on 12/7/17.
  */
 public class DBManager {
+
+
+    Connection conn;
+
+    public DBManager initializeJDBCDriver(String dbType, String fileName) throws ClassNotFoundException, SQLException {
+        Class.forName("org.hsqldb.jdbcDriver");
+        conn = DriverManager.getConnection(getConnectionStringFor(dbType, fileName), "sa", "");
+        return this;
+    }
+
+    private String getConnectionStringFor(String dbType, String fileName) {
+        String connectionString;
+        if (dbType.equalsIgnoreCase("file")) {
+            String temp = System.getProperty("user.dir") + File.separator + "abc" + File.separator + fileName;
+//            jdbcDataSource jdbcDataSource  =  new jdbcDataSource();
+            connectionString = String.format("jdbc:hsqldb:file:%s", temp);
+        } else {
+            connectionString = String.format("jdbc:hsqldb:mem:%s", fileName);
+        }
+        System.out.println(connectionString);
+        return connectionString;
+    }
+
+    public void shutDown() throws SQLException {
+        Statement statement = conn.createStatement();
+        statement.execute("SHUTDOWN");
+        statement.close();
+        conn.close();
+    }
+
+
+    public synchronized void query(String sql) throws SQLException {
+        Statement st = null;
+        ResultSet resultSet = null;
+
+        st = conn.createStatement();
+        resultSet = st.executeQuery(sql);
+        dump(resultSet);
+        st.close();
+    }
+
+    public static void dump(ResultSet rs) throws SQLException {
+
+        // the order of the rows in a cursor
+        // are implementation dependent unless you use the SQL ORDER statement
+        ResultSetMetaData meta = rs.getMetaData();
+        int colmax = meta.getColumnCount();
+        int i;
+        Object o = null;
+
+        // the result set is a cursor into the data.  You can only
+        // point to one row at a time
+        // assume we are pointing to BEFORE the first row
+        // rs.next() points to next row and returns true
+        // or false if there is no next row, which breaks the loop
+        for (; rs.next(); ) {
+            for (i = 0; i < colmax; ++i) {
+                o = rs.getObject(i + 1);    // Is SQL the first column is indexed
+
+                // with 1 not 0
+                System.out.print(o.toString() + " ");
+            }
+
+            System.out.println(" ");
+        }
+    }                                       //void dump( ResultSet rs )
+
+    public synchronized void update(String expression) throws SQLException {
+
+        Statement st = null;
+
+        st = conn.createStatement();    // statements
+
+        int i = st.executeUpdate(expression);    // run the query
+
+        if (i == -1) {
+            System.out.println("db error : " + expression);
+        }
+
+        st.close();
+    }
+
 }
--- src/test/java/DBManagerTest.java
+++ src/test/java/DBManagerTest.java
@@ -1,5 +1,54 @@
+import org.junit.Test;
+
+import java.sql.SQLException;
+
 /**
  * Created by rohitsehgal on 12/7/17.
  */
 public class DBManagerTest {
+
+    @Test
+    public void dbTest() {
+
+        DBManager db = null;
+
+        try {
+            db = new DBManager().initializeJDBCDriver("file","db_file");
+        } catch (Exception ex1) {
+            ex1.printStackTrace();    // could not start db
+
+            return;                   // bye bye
+        }
+
+        try {
+
+            db.update(
+                    "CREATE TABLE sample_table ( id INTEGER IDENTITY, str_col VARCHAR(256), num_col INTEGER)");
+        } catch (SQLException ex2) {
+
+            ex2.printStackTrace();
+        }
+
+        try {
+
+            // add some rows - will create duplicates if run more then once
+            // the id column is automatically generated
+            db.update(
+                    "INSERT INTO sample_table(str_col,num_col) VALUES('Ford', 100)");
+            db.update(
+                    "INSERT INTO sample_table(str_col,num_col) VALUES('Toyota', 200)");
+            db.update(
+                    "INSERT INTO sample_table(str_col,num_col) VALUES('Honda', 300)");
+            db.update(
+                    "INSERT INTO sample_table(str_col,num_col) VALUES('GM', 400)");
+
+            // do a query
+            db.query("SELECT * FROM sample_table WHERE num_col < 500");
+
+            // at end of program
+            db.shutDown();
+        } catch (SQLException ex3) {
+            ex3.printStackTrace();
+        }
+    }
 }
